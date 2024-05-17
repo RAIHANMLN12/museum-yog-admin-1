bbd5c2c605fb7c39cf66338fbb5ee463a78a9536
AsriAzhari123 <211111133@students.mikroskil.ac.id>
2024-05-18 02:42:43 +0700

    Perbaikan dan Penambahan
    Perbaikan pada bagian navbar, sidebar, dan dashboard.
    Penambahan pada folder components yaitu barchart.

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,62 +1,62 @@
 import React from 'react';
 import { MdOutlineLocationOn } from "react-icons/md";
 import Building from "../assets/building.png";
-import Dashboard from "../assets/icons/dashboard.png";
+import DashboardIcon from "../assets/icons/dashboard.png";
 import Ticket from "../assets/icons/ticket.png";
 import Events from "../assets/icons/events.png";
 import CMS from "../assets/icons/cms.png";
 import Review from "../assets/icons/review.png";
 
-const sidebar = () => {
+const Sidebar = () => {
     return (
-        <><div className='w-[280px] bg-[#5B6D54] fixed h-full px-4 py-2'>
-            <div className='text-white w-full my-2 mb-4 bg-[#5B6D54]'>
-                <div className="items-center w-full">
-                    <img src={Building} alt="building" className="w-14 h-14 justify-center item-center"/>
-                    <h1 className='text-[24px] font-bold text-center'>Museum</h1>
-                    <h1 className='text-[24px] font-bold mb-4 text-center'>Keraton Yogyakarta</h1>
+        <div className='fixed top-0 left-0 w-[280px] bg-[#5B6D54] h-full flex flex-col pt-20'>
+            <div className='text-white mb-4 px-4 py-2'>
+                <div className="text-center mb-4 pt-10">
+                    <img src={Building} alt="building" className="w-14 h-14 mx-auto"/>
+                    <h1 className='text-[24px] font-bold'>Museum</h1>
+                    <h1 className='text-[24px] font-bold mb-4'>Keraton Yogyakarta</h1>
                 </div>    
-                <div className='flex -ml-2 -mr-2'>
-                    <MdOutlineLocationOn className='w-8 h-8 -mt-2'/>
+                <div className='flex items-center'>
+                    <MdOutlineLocationOn className='w-8 h-8'/>
                     <h1 className='text-[12px] px-2'>Jl. Rotowijayan, Kadipaten, Kecamatan Kraton, Kota Yogyakarta, Daerah Istimewa Yogyakarta 55132</h1>
                 </div>
             </div>
-        </div><div className='w-[280px] mt-60 bg-[#7F9275] fixed h-full -mr-2 px-4 py-4'>
-                <ul className='text-white font-bold'>
-                    <li className='mb-2 rounded hover:shadow hover:bg-[#5B6D54] mt-2'>
-                        <a href="" className='flex'>
-                            <img src={Dashboard} alt="dashboard" className="w-6 h-6 ml-3 mt-3"/>
-                            <h1 className='text-[16px] font-medium ml-6 mb-4 mt-3'>Dashboard</h1>
+            <div className='bg-[#7F9275] flex-1 px-0 py-4'> {/* Remove left and right padding */}
+                <ul className='text-white font-bold space-y-2'>
+                    <li className='rounded hover:shadow hover:bg-[#5B6D54] mx-4'> {/* Add horizontal margin */}
+                        <a href="#" className='flex items-center p-2'>
+                            <img src={DashboardIcon} alt="dashboard" className="w-6 h-6"/>
+                            <h1 className='text-[16px] font-medium ml-4'>Dashboard</h1>
                         </a>
                     </li>
-                    <li className='mb-2 rounded hover:shadow hover:bg-[#5B6D54] mt-2'>
-                        <a href="" className='flex'>
-                            <img src={Ticket} alt="ticket" className="w-6 h-6 ml-3 mt-4"/>
-                            <h1 className='text-[16px] font-medium ml-6 mb-4 mt-3'>Ticket</h1>
+                    <li className='rounded hover:shadow hover:bg-[#5B6D54] mx-4'>
+                        <a href="#" className='flex items-center p-2'>
+                            <img src={Ticket} alt="ticket" className="w-6 h-6"/>
+                            <h1 className='text-[16px] font-medium ml-4'>Ticket</h1>
                         </a>
                     </li>
-                    <li className='mb-2 rounded hover:shadow hover:bg-[#5B6D54] mt-2'>
-                        <a href="" className='flex'>
-                            <img src={Events} alt="events" className="w-6 h-6 ml-3 mt-3"/>
-                            <h1 className='text-[16px] font-medium ml-6 mb-4 mt-3'>Events</h1>
+                    <li className='rounded hover:shadow hover:bg-[#5B6D54] mx-4'>
+                        <a href="#" className='flex items-center p-2'>
+                            <img src={Events} alt="events" className="w-6 h-6"/>
+                            <h1 className='text-[16px] font-medium ml-4'>Events</h1>
                         </a>
                     </li>
-                    <li className='mb-2 rounded hover:shadow hover:bg-[#5B6D54] mt-2'>
-                        <a href="" className='flex'>
-                            <img src={CMS} alt="cms" className="w-6 h-6 ml-3 mt-3"/>
-                            <h1 className='text-[16px] font-medium ml-6 mb-4 mt-3'>CMS</h1>
+                    <li className='rounded hover:shadow hover:bg-[#5B6D54] mx-4'>
+                        <a href="#" className='flex items-center p-2'>
+                            <img src={CMS} alt="cms" className="w-6 h-6"/>
+                            <h1 className='text-[16px] font-medium ml-4'>CMS</h1>
                         </a>
                     </li>
-                    <li className='mb-2 rounded hover:shadow hover:bg-[#5B6D54] mt-2'>
-                        <a href="" className='flex'>
-                            <img src={Review} alt="review" className="w-6 h-6 ml-3 mt-3"/>
-                            <h1 className='text-[16px] font-medium ml-6 mb-4 mt-3'>Review</h1>
+                    <li className='rounded hover:shadow hover:bg-[#5B6D54] mx-4'>
+                        <a href="#" className='flex items-center p-2'>
+                            <img src={Review} alt="review" className="w-6 h-6"/>
+                            <h1 className='text-[16px] font-medium ml-4'>Review</h1>
                         </a>
                     </li>
                 </ul>
-            </div></>
-        
-    )
+            </div>
+        </div>
+    );
 }
 
-export default sidebar
+export default Sidebar;
