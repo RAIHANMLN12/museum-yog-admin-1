f9df88dd54579fcce1a34c1e37176a66a78b4add
AsriAzhari123 <211111133@students.mikroskil.ac.id>
2024-05-21 01:12:54 +0700

    perbaikan dan penambahan

---
+++ src/pages/Ticket.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import Navbar from "../components/navbar";
+import Sidebar from "../components/sidebar";
+
+const users = [
+    {
+      name: "John Doe",
+      email: "jhon123@gmail.com",
+      image:
+        "https://th.bing.com/th/id/OIP.w6Cs6qz234c71XloeqKdwgHaHa?rs=1&pid=ImgDetMain",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane123@gmail.com",
+      image:
+        "https://th.bing.com/th/id/OIP.w6Cs6qz234c71XloeqKdwgHaHa?rs=1&pid=ImgDetMain",
+    },
+  ];
+
+const currentUser = users[1];
+
+export default function Ticket() {
+    return (
+        <div className="h-screen flex flex-col">
+        <Navbar
+            user={currentUser}
+            className="h-16 bg-gray-800 text-white flex items-center px-4"
+        />
+        <div className="flex flex-1 overflow-hidden">
+            <Sidebar />
+            <div className="flex-1 p-4 overflow-y-auto ml-[280px] mt-16 pt-10">
+            <h1 className="text-2xl font-bold">Ticket</h1>
+            </div>
+        </div>
+        </div>
+    );
+    }
+
+
+    
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,3 +7,5 @@ export {default as CmsPage} from "./CMS/CmsScreen";
 export {default as MuseumInformation} from "./CMS/MuseumInfo";
 export {default as Dashboard} from "./Dashboard";
 export {default as EditInformationScreen} from "./CMS/EditInfoScreen";
+export {default as Ticket} from "./Ticket";
+export {default as Events} from "./Event";
